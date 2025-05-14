a1d84cc2ad0e35d1bd74f3d25db31b9a08e6aa1e
Brijesh Kumar <bk10895@gmail.com>
2025-05-14 23:36:38 +0530

    feat: add subtitle to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="font-bold text-2xl">Namaste Youtube</h1>
+      <h3 className="font-medium">Youtube Profile</h3>
     </div>
   );
 }
